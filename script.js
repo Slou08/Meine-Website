3b12a97d78906f9ad292f5b3336f6ea0c8a400f3
Slou <slounity@gmail.com>
2022-11-27 00:33:29 +0100

    no

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ const listContainer = document.getElementsByClassName("list-container")[0]
 toggleButton.addEventListener("click", () =>{
     listContainer.classList.toggle("active")
 })
+
+const login = document.getElementById('login')
+
+login.innerHTML = 'Login'
+const user_is_logedin = false
+
+if(user_is_logedin) {
+    login.innerHTML = 'Konto'
+}
+
+alert('Du bist nicht angemeldet')
