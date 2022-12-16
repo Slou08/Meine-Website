6c1b11e51ab828ae3b6ec3c73b9ff6f7db273b52
Slou <slounity@gmail.com>
2022-12-16 18:18:06 +0100

    new design

--- script.js
+++
@@ -1,7 +0,0 @@
-const button1 = document.getElementById('button1')
-
-
-button1.addEventListener('click', () =>{
-    let name = document.querySelector('#name').value
-    alert('Hallo ' + name)
-})
